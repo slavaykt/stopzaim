82d78771ed5f97a6e5a245d0b8bb4d77c3fb1231
slavaykt <slava_ykt@mail.ru>
2021-04-11 00:18:10 +0900

    removed default value for column with type text

--- database/migrations/2021_01_25_042619_create_client_attachment_sections_table.php
+++ database/migrations/2021_01_25_042619_create_client_attachment_sections_table.php
@@ -16,7 +16,7 @@ class CreateClientAttachmentSectionsTable extends Migration
         Schema::create('client_attachment_sections', function (Blueprint $table) {
             $table->id();
             $table->string('Наименование',100)->default("");
-            $table->text('ТекстДляПечати',1000)->default("");
+            $table->text('ТекстДляПечати',1000);
             $table->string('ТекстПриложения')->default("");
             $table->string('ТекстОтсутствиеПриложения')->default("");
             $table->integer('Порядок');
