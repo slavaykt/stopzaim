f6dd1226cdb2b820be596da3ca9d7f020947cb28
slavaykt <slava_ykt@mail.ru>
2021-04-11 10:09:10 +0900

    removed redux devtools support

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -214,10 +214,12 @@ const App = () => {
   );
 }
 
-const store = createStore(rootReducer, compose(
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : compose
-));
+// const store = createStore(rootReducer, compose(
+//   applyMiddleware(thunk),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : compose
+// ));
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
