2213a574140405af5939924924771e1fd5f6e7a0
slavaykt <slava_ykt@mail.ru>
2021-04-11 10:14:31 +0900

    still fixing redux issue

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -219,7 +219,7 @@ const App = () => {
 //   window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : compose
 // ));
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
